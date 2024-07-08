0e1fb12ddc80a27b7a855fdecb3554eb45ae29fa
Stephan Rumswinkel <17520641+srwi@users.noreply.github.com>
2024-07-08 22:59:24 +0200

    Implement zoom features

--- src/main/kotlin/com/github/srwi/pycharmpixelglance/dialogs/ImageViewDialog.kt
+++ src/main/kotlin/com/github/srwi/pycharmpixelglance/dialogs/ImageViewDialog.kt
@@ -10,13 +10,18 @@ import com.intellij.ui.components.JBViewport
 import java.awt.BorderLayout
 import java.awt.Dimension
 import java.awt.Image
+import java.awt.Toolkit
+import java.awt.datatransfer.DataFlavor
+import java.awt.datatransfer.Transferable
+import java.awt.event.MouseEvent
+import java.awt.event.MouseMotionListener
+import java.awt.image.BufferedImage
 import java.io.ByteArrayInputStream
 import java.util.*
 import javax.imageio.ImageIO
-import javax.swing.ImageIcon
-import javax.swing.JComponent
-import javax.swing.JLabel
-import javax.swing.JPanel
+import javax.swing.*
+import kotlin.math.max
+import kotlin.math.min
 
 class ImageViewDialog(
     project: Project?,
@@ -25,10 +30,17 @@ class ImageViewDialog(
 ) : DialogWrapper(project) {
 
     private val imageLabel: JLabel
+    private val originalImage: BufferedImage
+    private var currentZoom: Double = 1.0
+    private val scrollPane: JBScrollPane
+    private val footerLabel: JLabel
 
     init {
         this.title = title
-        imageLabel = JLabel(ImageIcon(base64ToImage(base64ImageString)))
+        originalImage = base64ToImage(base64ImageString)
+        imageLabel = JLabel(ImageIcon(originalImage))
+        scrollPane = JBScrollPane(imageLabel)
+        footerLabel = JLabel(" ")
         init()
     }
 
@@ -38,13 +50,53 @@ class ImageViewDialog(
         val toolbar = createToolbar()
         panel.add(toolbar, BorderLayout.NORTH)
 
-        val scrollPane = JBScrollPane(imageLabel)
         scrollPane.preferredSize = Dimension(600, 400)
 
         val viewport = JBViewport()
         viewport.add(scrollPane, BorderLayout.CENTER)
 
         panel.add(viewport, BorderLayout.CENTER)
+        panel.add(footerLabel, BorderLayout.SOUTH)
+
+        imageLabel.addMouseMotionListener(object : MouseMotionListener {
+            override fun mouseDragged(e: MouseEvent?) {
+                updateFooter(e)
+            }
+
+            override fun mouseMoved(e: MouseEvent) {
+                updateFooter(e)
+            }
+
+            private fun updateFooter(e: MouseEvent?) {
+                e?.let {
+                    val mouseX = e.x
+                    val mouseY = e.y
+
+                    val icon = imageLabel.icon as ImageIcon
+                    val iconWidth = icon.iconWidth
+                    val iconHeight = icon.iconHeight
+
+                    val viewport = scrollPane.viewport
+                    val viewPosition = viewport.viewPosition
+
+                    val xOffset = max((viewport.width - iconWidth) / 2 - viewPosition.x, 0)
+                    val yOffset = max((viewport.height - iconHeight) / 2 - viewPosition.y, 0)
+
+                    val imgX = ((mouseX - xOffset) / currentZoom).toInt()
+                    val imgY = ((mouseY - yOffset) / currentZoom).toInt()
+
+                    if (imgX in 0 until originalImage.width && imgY in 0 until originalImage.height) {
+                        val pixel = originalImage.getRGB(imgX, imgY)
+                        val r = (pixel shr 16) and 0xFF
+                        val g = (pixel shr 8) and 0xFF
+                        val b = pixel and 0xFF
+                        footerLabel.text = "x=$imgX, y=$imgY, value=($r, $g, $b)"
+                    } else {
+                        footerLabel.text = " "
+                    }
+                }
+            }
+        })
 
         return panel
     }
@@ -66,38 +118,94 @@ class ImageViewDialog(
 
     private inner class CopyAction : DumbAwareAction("Copy", "Copy image to clipboard", AllIcons.Actions.Copy) {
         override fun actionPerformed(e: AnActionEvent) {
+            val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+            clipboard.setContents(TransferableImage(originalImage), null)
         }
     }
 
     private inner class SaveAction : DumbAwareAction("Save...", "Save image to file", AllIcons.Actions.MenuSaveall) {
         override fun actionPerformed(e: AnActionEvent) {
+            val fileChooser = JFileChooser()
+            fileChooser.dialogTitle = "Save Image"
+            fileChooser.fileSelectionMode = JFileChooser.FILES_ONLY
+
+            val userSelection = fileChooser.showSaveDialog(null)
+            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                val fileToSave = fileChooser.selectedFile
+                ImageIO.write(originalImage, "png", fileToSave)
+            }
         }
     }
 
     private inner class ZoomInAction : DumbAwareAction("Zoom In", "Zoom in", AllIcons.General.ZoomIn) {
         override fun actionPerformed(e: AnActionEvent) {
+            zoom(1.25)
         }
     }
 
     private inner class ZoomOutAction : DumbAwareAction("Zoom Out", "Zoom out", AllIcons.General.ZoomOut) {
         override fun actionPerformed(e: AnActionEvent) {
+            zoom(0.8)
         }
     }
 
     private inner class FitToWindowAction : DumbAwareAction("Fit to Window", "Fit image to window size", AllIcons.General.FitContent) {
         override fun actionPerformed(e: AnActionEvent) {
+            fitToWindow()
         }
     }
 
     private inner class OriginalSizeAction : DumbAwareAction("Original Size", "Reset to original size", AllIcons.General.ActualZoom) {
         override fun actionPerformed(e: AnActionEvent) {
+            resetZoom()
         }
     }
 
-    private fun base64ToImage(base64String: String): Image {
+    private fun base64ToImage(base64String: String): BufferedImage {
         val imageBytes = Base64.getDecoder().decode(base64String)
         return ByteArrayInputStream(imageBytes).use {
             ImageIO.read(it)
+        }
+    }
+
+    private fun zoom(factor: Double) {
+        currentZoom *= factor
+        updateImage()
+    }
+
+    private fun fitToWindow() {
+        val viewportSize = scrollPane.viewport.size
+        val widthRatio = viewportSize.width.toDouble() / originalImage.width
+        val heightRatio = viewportSize.height.toDouble() / originalImage.height
+        currentZoom = min(widthRatio, heightRatio)
+        updateImage()
+    }
+
+    private fun resetZoom() {
+        currentZoom = 1.0
+        updateImage()
+    }
+
+    private fun updateImage() {
+        val newWidth = (originalImage.width * currentZoom).toInt()
+        val newHeight = (originalImage.height * currentZoom).toInt()
+        val resizedImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_DEFAULT)
+        imageLabel.icon = ImageIcon(resizedImage)
+        imageLabel.revalidate()
+        imageLabel.repaint()
+    }
+
+    private class TransferableImage(private val image: BufferedImage) : Transferable {
+        override fun getTransferData(flavor: DataFlavor?): Any? {
+            return if (flavor == DataFlavor.imageFlavor) image else null
+        }
+
+        override fun isDataFlavorSupported(flavor: DataFlavor?): Boolean {
+            return flavor == DataFlavor.imageFlavor
+        }
+
+        override fun getTransferDataFlavors(): Array<DataFlavor> {
+            return arrayOf(DataFlavor.imageFlavor)
         }
     }
 }
